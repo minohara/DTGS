d2fcb6dba48e54dc856b530df23a20a38ede9122
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2021-09-02 15:06:22 +0900

    Connect to 2 server

--- Client.java
+++ Client.java
@@ -7,19 +7,27 @@ import java.net.*;
 public class Client {
   BufferedReader in;
   PrintWriter out;
+  BufferedReader in2;
+  PrintWriter out2;
   char name;
 
 
   public ArrayList<String> cards;
   String n = "3456789TJQKA2";
 
-  public Client(String server, int port) throws Exception {
+  public Client(String server, int port,String server2, int port2) throws Exception {
     Socket s = new Socket(server, port);
+    Socket s2 = new Socket(server2, port2);
     in = new BufferedReader(new InputStreamReader( s.getInputStream()));
+    in2 = new BufferedReader(new InputStreamReader( s2.getInputStream()));
     out = new PrintWriter(s.getOutputStream(),true /* autoFlush */);
+    out2 = new PrintWriter(s2.getOutputStream(),true /* autoFlush */);
     cards = new ArrayList<String>();
     while ( true ) {
       String str = in.readLine();
+      //System.out.println(str);
+      in2.readLine();
+      //System.out.println("2" + str2);
       String[] cmd = str.split("\s");
       if ( cmd[0].equals("CN") ) {
         name = cmd[1].charAt(0);
@@ -43,6 +51,7 @@ public class Client {
       else if ( cmd[0].equals("WN") ) {
         System.out.format("%s さんの勝ちです\n", cmd[1]);
         s.close();
+        s2.close();
         break;
       }
       else {
@@ -54,9 +63,11 @@ public class Client {
   void play(String card) {
     if ( card.equals("0") ) {
       out.format("PC\n");
+      out2.format("PC\n");
     }
     else {
       out.format("PC %s\n", card);
+      out2.format("PC %s\n", card);
     }
   }
 
@@ -105,6 +116,14 @@ public class Client {
     if (args.length > 1) {
       port = Integer.parseInt(args[1]);
     }
-    Client client = new Client(server, port);
+    String server2 = "localhost";
+    if (args.length > 2) {
+      server2 = args[2];
+    }
+    int port2 = 8190;
+    if (args.length > 3) {
+      port = Integer.parseInt(args[3]);
+    }
+    Client client = new Client(server, port,server2, port2);
   }
 }
